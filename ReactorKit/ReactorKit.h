24417810e8a26d155581da1bc0665d94bdd231a7
Tetsuya Hirano <tetsuya@wantedly.com>
2021-05-07 19:08:16 +0900

    add project

---
+++ ReactorKit/ReactorKit.h
@@ -0,0 +1,18 @@
+//
+//  ReactorKit.h
+//  ReactorKit
+//
+//  Created by Tetsuya Hirano on 2021/05/07.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReactorKit.
+FOUNDATION_EXPORT double ReactorKitVersionNumber;
+
+//! Project version string for ReactorKit.
+FOUNDATION_EXPORT const unsigned char ReactorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactorKit/PublicHeader.h>
+
+
